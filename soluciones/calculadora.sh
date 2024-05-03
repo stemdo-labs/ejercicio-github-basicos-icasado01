412ea8da97da1f936bf2d7ffe31cba7040feefb8
icasado01 <166407751+icasado01@users.noreply.github.com>
2024-05-03 09:57:24 +0200

    Update calculadora.sh

--- soluciones/calculadora.sh
+++ soluciones/calculadora.sh
@@ -4,23 +4,33 @@ operacion=$1
 num1=$2
 num2=$3
 
+# Verificar si los valores ingresados son números
+if ! [[ $num1 =~ ^[0-9]+$ ]]; then
+  echo "El primer número no es un número válido."
+  exit 1
+fi
+
+if ! [[ $num2 =~ ^[0-9]+$ ]]; then
+  echo "El segundo número no es un número válido."
+  exit 1
+fi
 
 case $operacion in
   suma)
-    resultado=$((valor1 + valor2))
+    resultado=$((num1 + num2))
     ;;
   resta)
-    resultado=$((valor1 - valor2))
+    resultado=$((num1 - num2))
     ;;
   multiplicacion)
-    resultado=$((valor1 * valor2))
+    resultado=$((num1 * num2))
     ;;
   division)
     if [ $valor2 -eq 0 ]; then
       echo "No se puede dividir por cero."
       exit 1
     fi
-    resultado=$((valor1 / valor2))
+    resultado=$((num1 / num2))
     ;;
   *)
     echo "Operación no válida. Las operaciones válidas son: suma, resta, multiplicacion, division."
